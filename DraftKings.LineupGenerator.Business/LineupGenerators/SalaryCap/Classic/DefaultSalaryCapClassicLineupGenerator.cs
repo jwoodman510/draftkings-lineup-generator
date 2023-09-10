bb393ffc120dc538612e900290b42e0b4c404171
Jordan Woodman <jordan.woodman@live.com>
2023-09-10 12:54:09 -0400

    remove unused using

--- DraftKings.LineupGenerator.Business/LineupGenerators/SalaryCap/Classic/DefaultSalaryCapClassicLineupGenerator.cs
+++ DraftKings.LineupGenerator.Business/LineupGenerators/SalaryCap/Classic/DefaultSalaryCapClassicLineupGenerator.cs
@@ -11,7 +11,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
