5d63633846e4760408cefa3d5765f582b0471e0a
Jordan Woodman <jordan.woodman@live.com>
2023-09-25 14:12:13 -0400

    Fix min fppg filter

--- DraftKings.LineupGenerator.Business/Filters/DraftStatFilter.cs
+++ DraftKings.LineupGenerator.Business/Filters/DraftStatFilter.cs
@@ -14,7 +14,13 @@ namespace DraftKings.LineupGenerator.Business.Filters
 
         public static IEnumerable<DraftableModel> MinimumFppg(this IEnumerable<DraftableModel> draftables, List<DraftStatModel> draftStats, decimal fppg)
         {
-            return draftables.Where(x => x.GetFppg(draftStats) >= fppg);
+            return draftables.GroupBy(x => x.Position)
+                .SelectMany(positionGroup =>
+                {
+                    return positionGroup.Any(y => y.GetFppg(draftStats) >= fppg)
+                      ? positionGroup.Where(y => y.GetFppg(draftStats) >= fppg)
+                      : positionGroup;
+                });
         }
     }
 }
