79d0111639cff68bbd4a4f032fb3874ad92f50e1
Jordan Woodman <jordan.woodman@live.com>
2023-11-18 08:10:08 -0500

    add maui project

---
+++ DraftKings.LineupGenerator.UI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace DraftKings.LineupGenerator.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DraftKings.LineupGenerator.UI/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace DraftKings.LineupGenerator.UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
