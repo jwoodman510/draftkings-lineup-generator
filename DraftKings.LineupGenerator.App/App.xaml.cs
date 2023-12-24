1723a51a0a570dbe412e263d15e6fbe17ac88126
Jordan Woodman <jordan.woodman@live.com>
2023-12-24 11:43:19 -0500

    Add razor class library & maui blazor hybrid app

---
+++ DraftKings.LineupGenerator.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DraftKings.LineupGenerator.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DraftKings.LineupGenerator.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DraftKings.LineupGenerator.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
