c83b83530a2e75e688ee9c6d8380c21dc4f2bd98
Jordan Woodman <jordan.woodman@live.com>
2023-12-31 09:11:59 -0500

    Add internet connectivity check

--- DraftKings.LineupGenerator.App/MainPage.xaml.cs
+++ DraftKings.LineupGenerator.App/MainPage.xaml.cs
@@ -5,6 +5,19 @@
         public MainPage()
         {
             InitializeComponent();
+
+            rootComponent.Parameters = new Dictionary<string, object?>
+            {
+                { "IsConnected", Connectivity.Current.NetworkAccess == NetworkAccess.Internet }
+            };
+
+            Connectivity.Current.ConnectivityChanged += (object sender, ConnectivityChangedEventArgs args) =>
+            {
+                rootComponent.Parameters = new Dictionary<string, object?>
+                {
+                    { "IsConnected", Connectivity.Current.NetworkAccess == NetworkAccess.Internet }
+                };
+            };
         }
     }
 }
