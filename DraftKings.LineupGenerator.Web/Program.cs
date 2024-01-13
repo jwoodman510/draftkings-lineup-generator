6e77aaa408560c49e4eded9148ba6e84b404c138
jwoodman510 <jordan.woodman@live.com>
2024-01-13 12:43:28 -0500

    Dev blazor wasm (#15)
    * Add Blazor WASM app

---
+++ DraftKings.LineupGenerator.Web/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using DraftKings.LineupGenerator.Web;
+using DraftKings.LineupGenerator.Business;
+using DraftKings.LineupGenerator.Razor;
+using DraftKings.LineupGenerator.Caching;
+using DraftKings.LineupGenerator.Razor.Services;
+using Microsoft.Extensions.Http;
+using DraftKings.LineupGenerator.Web.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+    .RegisterServices()
+    .RegisterRazorServices()
+    .AddScoped<ICacheService, LocalStorageCacheService>()
+    .Configure<HttpClientFactoryOptions>(options =>
+    {
+        options.HttpMessageHandlerBuilderActions.Add(builder =>
+        {
+            builder.AdditionalHandlers.Add(new ProxyHttpClientHandler());
+        });
+    });
+
+await builder.Build().RunAsync();
