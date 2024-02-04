10b27c66c82be75f658a4be7fe313b9f7e50e058
Jordan Woodman <jordan.woodman@live.com>
2024-02-04 11:48:26 -0500

    Add gzip compression to local storage

--- DraftKings.LineupGenerator.Razor/ServiceRegistry.cs
+++ DraftKings.LineupGenerator.Razor/ServiceRegistry.cs
@@ -1,7 +1,9 @@
 ﻿using Blazored.LocalStorage;
+using Blazored.LocalStorage.Serialization;
 using DraftKings.LineupGenerator.Razor.Contests;
 using DraftKings.LineupGenerator.Razor.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace DraftKings.LineupGenerator.Razor
 {
@@ -10,11 +12,19 @@ namespace DraftKings.LineupGenerator.Razor
         public static IServiceCollection RegisterRazorServices(this IServiceCollection services)
         {
             return services
+                .AddLocalStorage()
                 .AddBlazorBootstrap()
-                .AddBlazoredLocalStorage()
                 .AddScoped<ContestStateProvider>()
                 .AddTransient<IRecentContestService, RecentContestService>()
                 .AddTransient<IContestHistoryService, ContestHistoryService>();
+        }
+
+        private static IServiceCollection AddLocalStorage(this IServiceCollection services)
+        {
+            services.AddBlazoredLocalStorage();
+            services.Replace(ServiceDescriptor.Scoped<IJsonSerializer, LocalStorageSerializer>());
+
+            return services;
         }
     }
 }
